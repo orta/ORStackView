00eaa79f5de061e7c6fe7c03a693a118285569f7
Denis Lebedev <d2.lebedev@gmail.com>
2015-04-26 16:09:34 +0300

    Add dirty version of ORHorizontalStackView

---
+++ Classes/ios/ORHorizontalStackView.m
@@ -0,0 +1,251 @@
+//
+//  ORHorizontalStackView.m
+//  ORStackView
+//
+//  Created by Dzianis Lebedzeu on 4/23/15.
+//
+//
+
+#import "ORHorizontalStackView.h"
+#import <FLKAutoLayout/UIView+FLKAutoLayout.h>
+#import "ORStackView+Private.h"
+
+@interface ORHorizontalStackView()
+
+/// Delay updating constraints when true
+@property (nonatomic, assign) BOOL batchingUpdates;
+@property (nonatomic, strong) NSLayoutConstraint *trailingConstraint;
+
+
+// TODO: move to private
+
+/// An array of StackView objects in the order they should be shown
+@property (nonatomic, strong) NSMutableArray *viewStack;
+
+@end
+
+@implementation ORHorizontalStackView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    
+    [self _setup];
+    
+    return self;
+}
+
+- (instancetype)init
+{
+    return [self initWithFrame:CGRectZero];
+}
+
+- (void)awakeFromNib
+{
+    [self _setup];
+}
+
+- (void)_setup
+{
+    _viewStack = [NSMutableArray array];
+    _trailingMarginWidth = 0;
+}
+
+- (void)updateConstraints
+{
+    // Remove all constraints
+    for (StackView *stackView in self.viewStack) {
+        [self removeConstraint:stackView.topConstraint];
+    }
+    
+    // Add the new constraints
+    for (StackView *stackView in self.viewStack) {
+        UIView *view = stackView.view;
+        NSString *predicate = @(stackView.constant).stringValue;
+        NSInteger index = [self.viewStack indexOfObject:stackView];
+        
+        if (index == 0) {
+            stackView.topConstraint = [[view alignLeadingEdgeWithView:self predicate:predicate] lastObject];
+        } else {
+            UIView *viewAbove = [self.viewStack[index - 1] view];
+            stackView.topConstraint = [[view constrainLeadingSpaceToView:viewAbove predicate:predicate] lastObject];
+        }
+    }
+    
+    if (self.trailingMarginWidth != NSNotFound) {
+        [self removeConstraint:self.trailingConstraint];
+        UIView *lastView = self.lastView;
+        if (self.lastView) {
+            NSString *constraint = [NSString stringWithFormat:@"%0.0f", self.trailingMarginWidth];
+            self.trailingConstraint = [[self alignTrailingEdgeWithView:lastView predicate:constraint] lastObject];
+        }
+    }
+    
+    [super updateConstraints];
+}
+
+#pragma mark - Adding Subviews
+
+- (void)addSubview:(UIView *)view withTopMargin:(CGFloat)margin
+{
+    [self _addSubview:view withTopMargin:margin centered:NO sideMargin:0];
+}
+
+- (void)addSubview:(UIView *)view withTopMargin:(CGFloat)topMargin sideMargin:(CGFloat)sideMargin
+{
+    [self _addSubview:view withTopMargin:topMargin centered:YES sideMargin:sideMargin];
+}
+
+- (void)_addSubview:(UIView *)view withTopMargin:(CGFloat)topMargin centered:(BOOL)centered sideMargin:(CGFloat)sideMargin
+{
+    NSInteger index = self.viewStack.count;
+    [self _insertSubview:view atIndex:index withTopMargin:topMargin centered:centered sideMargin:sideMargin];
+}
+
+#pragma mark - Inserting Subviews
+
+- (void)insertSubview:(UIView *)view atIndex:(NSInteger)index withTopMargin:(CGFloat)margin;
+{
+    [self _insertSubview:view atIndex:index withTopMargin:margin centered:NO sideMargin:0];
+}
+
+- (void)insertSubview:(UIView *)view atIndex:(NSInteger)index withTopMargin:(CGFloat)topMargin sideMargin:(CGFloat)sideMargin
+{
+    [self _insertSubview:view atIndex:index withTopMargin:topMargin centered:YES sideMargin:sideMargin];
+}
+
+
+- (void)insertSubview:(UIView *)view belowSubview:(UIView *)siblingSubview withTopMargin:(CGFloat)margin
+{
+    BOOL hasSibling = [self.subviews containsObject:siblingSubview];
+    NSInteger index = hasSibling ? [self indexOfView:siblingSubview] : self.viewStack.count;
+    [self _insertSubview:view atIndex:index withTopMargin:margin centered:NO sideMargin:0];
+}
+
+- (void)insertSubview:(UIView *)view aboveSubview:(UIView *)siblingSubview withTopMargin:(CGFloat)margin
+{
+    NSAssert([self.subviews containsObject:siblingSubview], @"SiblingSubview not found in ORStackView");
+    
+    NSInteger index = [self indexOfView:siblingSubview] - 1;
+    [self _insertSubview:view atIndex:index withTopMargin:margin centered:NO sideMargin:0];
+}
+
+- (void)_insertSubview:(UIView *)view atIndex:(NSInteger)index withTopMargin:(CGFloat)topMargin centered:(BOOL)centered sideMargin:(CGFloat)sideMargin
+{
+    NSParameterAssert(view);
+    if ([self.subviews containsObject:view]) return;
+    
+    [super addSubview:view];
+    
+    StackView *stackView = [[StackView alloc] init];
+    stackView.view = view;
+    stackView.constant = topMargin;
+    [self.viewStack insertObject:stackView atIndex:index];
+    
+   // NSString *vhl = [NSString stringWithFormat:@"V:[topLayoutGuide]-%@-[view]", predicate];
+   // NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:vhl options:NSLayoutFormatDirectionLeadingToTrailing metrics:nil views:NSDictionaryOfVariableBindings(view, topLayoutGuide)];
+   // [self addConstraints:constraints];
+  //  stackView.topConstraint = [constraints firstObject];
+  
+    if (centered) {
+        //TODO: handle topLayoutGuide here
+        [view constrainHeightToView:self predicate:@(-sideMargin).stringValue];
+        [view alignCenterYWithView:self predicate:nil];
+    }
+    
+    if (!self.batchingUpdates) [self setNeedsUpdateConstraints];
+}
+
+#pragma mark Removal
+
+- (void)removeSubview:(UIView *)subview
+{
+    if (![self.subviews containsObject:subview]) return;
+    
+    [subview removeFromSuperview];
+    
+    for (StackView *stackView in self.viewStack.copy) {
+        if ([subview isEqual:stackView.view]) {
+            [self.viewStack removeObject:stackView];
+        }
+    }
+    
+    if (!self.batchingUpdates) [self setNeedsUpdateConstraints];
+}
+
+- (void)removeAllSubviews
+{
+    for (StackView *stackView in [self.viewStack copy]) {
+        [self.viewStack removeObject:stackView];
+        [stackView.view removeFromSuperview];
+    }
+    
+    if (!self.batchingUpdates) [self setNeedsUpdateConstraints];
+}
+
+#pragma mark Batching
+
+- (void)performBatchUpdates:(void (^)(void))updates;
+{
+    NSParameterAssert(updates);
+    
+    self.batchingUpdates = YES;
+    updates();
+    self.batchingUpdates = NO;
+    
+    [self setNeedsUpdateConstraints];
+}
+
+- (void)addViewController:(UIViewController *)viewController toParent:(UIViewController *)parentViewController withTopMargin:(CGFloat)margin
+{
+    [viewController willMoveToParentViewController:parentViewController];
+    [parentViewController addChildViewController:viewController];
+    [self addSubview:viewController.view withTopMargin:margin];
+    [viewController didMoveToParentViewController:parentViewController];
+}
+
+- (void)addViewController:(UIViewController *)viewController toParent:(UIViewController *)parentViewController withTopMargin:(CGFloat)margin sideMargin:(CGFloat)sideMargin;
+{
+    [viewController willMoveToParentViewController:parentViewController];
+    [parentViewController addChildViewController:viewController];
+    [self addSubview:viewController.view withTopMargin:margin sideMargin:sideMargin];
+    [viewController didMoveToParentViewController:parentViewController];
+}
+
+
+#pragma mark Helper functions
+
+- (NSInteger)indexOfView:(UIView *)view
+{
+    for (StackView *stackView in self.viewStack) {
+        if ([view isEqual:stackView.view]) {
+            return [self.viewStack indexOfObject:stackView];
+        }
+    }
+    return NSNotFound;
+}
+
+- (NSLayoutConstraint *)topConstraintForView:(UIView *)view
+{
+    for (StackView *stackView in self.viewStack) {
+        if ([view isEqual:stackView.view]) {
+            return stackView.topConstraint;
+        }
+    }
+    return nil;
+}
+
+- (UIView *)lastView
+{
+    return [[self.viewStack lastObject] view];
+}
+
+#pragma mark - Layout Guides
+
+- (void)setTopLayoutGuide:(id<UILayoutSupport>)topLayoutGuide {
+    _topLayoutGuide = topLayoutGuide;
+    [self needsUpdateConstraints];
+}
+
+@end
--- ORStackViewExample/ORFirstViewController.m
+++ ORStackViewExample/ORFirstViewController.m
@@ -8,12 +8,12 @@
 
 #import "ORFirstViewController.h"
 #import "ORColourView.h"
-#import <ORStackView/ORStackView.h>
+#import <ORStackView/ORHorizontalStackView.h>
 
 // Simplest use case: Adding views in order
 
 @interface ORFirstViewController ()
-@property (nonatomic, strong) ORStackView *stackView;
+@property (nonatomic, strong) ORHorizontalStackView *stackView;
 @end
 
 @implementation ORFirstViewController
@@ -29,31 +29,41 @@
 
 - (void)viewDidLoad
 {
-    self.stackView = [[ORStackView alloc] init];
+    self.stackView = [[ORHorizontalStackView alloc] init];
     [self.view addSubview:self.stackView];
     if ([self respondsToSelector:@selector(topLayoutGuide)]) {
         [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0]];
+    } else {
+        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
     }
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0]];
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0]];
+    
+    if ([self respondsToSelector:@selector(bottomLayoutGuide)]) {
+        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomLayoutGuide attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
+    } else {
+        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0]];
+    }
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.stackView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0]];
+    
+
 
     ORColourView *view1 = [[ORColourView alloc] init];
-    view1.text = @"ORStackView - Tap Me";
-    view1.fakeContentSize = (CGSize){ UIViewNoIntrinsicMetric , 40};
+    view1.text = @"ORHorizontalStackView - Tap Me";
+    view1.fakeContentSize = (CGSize){ 40, UIViewNoIntrinsicMetric};
 
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(addView)];
     [view1 addGestureRecognizer:tapGesture];
 
     ORColourView *view2 = [[ORColourView alloc] init];
     view2.text = @"Subtitle";
-    view2.fakeContentSize = (CGSize){ UIViewNoIntrinsicMetric , 20 };
+    view2.fakeContentSize = (CGSize){ 30, UIViewNoIntrinsicMetric};
 
     ORColourView *view3 = [[ORColourView alloc] init];
-    view3.text = @"By default, new subviews are added to the bottom of ORStackView.";
-    view3.fakeContentSize = (CGSize){ UIViewNoIntrinsicMetric , 100 };
+    view3.text = @"By default, new subviews are added to the right of ORHorizontalStackView.";
+    view3.fakeContentSize = (CGSize){ 100, UIViewNoIntrinsicMetric };
 
     [self.stackView addSubview:view1 withTopMargin:20.0 sideMargin:30.0];
-    [self.stackView addSubview:view2 withTopMargin:40.0 sideMargin:70.0];
+    [self.stackView addSubview:view2 withTopMargin:10.0 sideMargin:170.0];
     [self.stackView addSubview:view3 withTopMargin:30.0 sideMargin:20.0];
 }
 
@@ -61,7 +71,7 @@
 {
     ORColourView *view = [[ORColourView alloc] init];
     view.text = @"Tap to remove";
-    view.fakeContentSize = (CGSize){ UIViewNoIntrinsicMetric , 24 };
+    view.fakeContentSize = (CGSize){ 24, UIViewNoIntrinsicMetric};
     [self.stackView addSubview:view withTopMargin:5.0 sideMargin:40.0];
 
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeTappedView:)];
